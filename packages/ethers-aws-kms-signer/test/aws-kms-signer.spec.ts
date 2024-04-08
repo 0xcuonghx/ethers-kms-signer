ee452d9d785106eec7790542179136b204f8b2de
Manuel Meinert <m.meinert@21.finance>
2024-04-08 12:23:51 +0200

    feat: allow AWS credetials from credential provider

--- packages/ethers-aws-kms-signer/test/aws-kms-signer.spec.ts
+++ packages/ethers-aws-kms-signer/test/aws-kms-signer.spec.ts
@@ -9,38 +9,64 @@ dotenv.config();
 context("AwsKmsSigner", () => {
   let signer: AwsKmsSigner;
 
-  beforeEach(() => {
-    signer = new AwsKmsSigner({
-      keyId: process.env.TEST_KMS_KEY_ID!,
-      region: process.env.TEST_KMS_REGION_ID!,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-      },
+  describe("explicit credentials", () => {
+    beforeEach(() => {
+      signer = new AwsKmsSigner({
+        keyId: process.env.TEST_KMS_KEY_ID!,
+        region: process.env.TEST_KMS_REGION_ID!,
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        },
+      });
+    });
+
+    it("Should return correct public key", async () => {
+      expect(await signer.getAddress()).to.eql(
+        "0x27d30941a21923e25a7429e3e576e9609c012a27"
+      );
+    });
+
+    it("Should get sign a message", async () => {
+      const testMessage = "test";
+      const publicAddress = await signer.getAddress();
+
+      const signature = await signer.signMessage(testMessage);
+
+      const eip191Hash = solidityPackedKeccak256(
+        ["string", "string"],
+        ["\x19Ethereum Signed Message:\n4", testMessage]
+      );
+
+      const recoveredAddress = recoverAddress(eip191Hash, signature);
+
+      expect(recoveredAddress.toLowerCase()).to.equal(
+        publicAddress.toLowerCase()
+      );
     });
   });
 
-  it("Should return correct public key", async () => {
-    expect(await signer.getAddress()).to.eql(
-      "0x27d30941a21923e25a7429e3e576e9609c012a27"
-    );
-  });
+  describe("AWS SSO", () => {
+    it("Should get sign a message", async () => {
+      signer = new AwsKmsSigner({
+        keyId: process.env.TEST_KMS_KEY_ID!,
+        region: process.env.TEST_KMS_REGION_ID!,
+      });
+      const testMessage = "test";
+      const publicAddress = await signer.getAddress();
 
-  it("Should get sign a message", async () => {
-    const testMessage = "test";
-    const publicAddress = await signer.getAddress();
+      const signature = await signer.signMessage(testMessage);
 
-    const signature = await signer.signMessage(testMessage);
+      const eip191Hash = solidityPackedKeccak256(
+        ["string", "string"],
+        ["\x19Ethereum Signed Message:\n4", testMessage]
+      );
 
-    const eip191Hash = solidityPackedKeccak256(
-      ["string", "string"],
-      ["\x19Ethereum Signed Message:\n4", testMessage]
-    );
+      const recoveredAddress = recoverAddress(eip191Hash, signature);
 
-    const recoveredAddress = recoverAddress(eip191Hash, signature);
-
-    expect(recoveredAddress.toLowerCase()).to.equal(
-      publicAddress.toLowerCase()
-    );
+      expect(recoveredAddress.toLowerCase()).to.equal(
+        publicAddress.toLowerCase()
+      );
+    });
   });
 });
